95658de4d899e8572159b9dc3d1990bb906486a7
Michael Altmann <mic.altmann@gmail.com>
2019-07-01 22:11:46 +0200

    Add result into ValidateableParam
    Problem: If combination validation logic returns Success but secondAddress is not valid then SecondAddress set to null and customerResult is success which is wrong. It should be failed.

--- DddSamples/SolutionTests.cs
+++ DddSamples/SolutionTests.cs
@@ -1,22 +1,42 @@
 using CSharpFunctionalExtensions;
 using FluentAssertions;
 using NUnit.Framework;
+using System;
 
 namespace Tests.Solution
 {
     public class ValidateableParam<T>
     {
-        public T Value { get; private set; }
+        private Result<T> _result;
 
-        public bool IsValid => Value != null;
+        public T Value => _result.Value;
 
-        public ValidateableParam(T value)
+        public string Error => _result.Error;
+
+        public bool IsValid => _result.IsSuccess;
+        
+        public ValidateableParam(Result<T> result)
         {
-            Value = value;
+            _result = result;
+        }
+
+        public static implicit operator Result<T> (ValidateableParam<T> validateableParam)
+        {
+            return validateableParam.IsValid
+                ? Result.Ok(validateableParam.Value)
+                : Result.Fail<T>(validateableParam.Error);
+        }
+
+        public static implicit operator Result(ValidateableParam<T> validateableParam)
+        {
+            return validateableParam.IsValid
+                ? Result.Ok(validateableParam.Value)
+                : Result.Fail<T>(validateableParam.Error);
         }
 
         public static implicit operator ValidateableParam<T>(Result<T> result)
         {
+
             return result.ToValidateableParam();
         }
     }
@@ -25,9 +45,7 @@ namespace Tests.Solution
     {
         public static ValidateableParam<T> ToValidateableParam<T>(this Result<T> result)
         {
-            return result.IsFailure
-                ? new ValidateableParam<T>(default(T))
-                : new ValidateableParam<T>(result.Value);
+            return new ValidateableParam<T>(result);
         }
     }
 
@@ -45,17 +63,42 @@ namespace Tests.Solution
             SecondAddress = secondAddress;
         }
 
-        public static Result Create(ValidateableParam<CustomerName> name,
+        private static bool IfNameStartsWithAThenStreetAlsoShouldStartWithA(CustomerName name,
+                Address mainAddress)
+        {
+            return name.Value.StartsWith("A") && mainAddress.Street.StartsWith("A");
+        }
+
+        public static Result<Customer> Create(ValidateableParam<CustomerName> name,
             ValidateableParam<Address> mainAddress,
             ValidateableParam<Address> secondAddress)
         {
-            if (name.IsValid && name.Value.Value.StartsWith("A")
-                && mainAddress.IsValid && !mainAddress.Value.Street.StartsWith("A"))
-                return Result.Fail("If name starts with A then street should also start with A.");
+            var nameMainAddressCombinationResult = Validate(name, mainAddress,
+                IfNameStartsWithAThenStreetAlsoShouldStartWithA, 
+                "If name starts with A then street should also start with A.");
+            
+            var result = Result.Combine(nameMainAddressCombinationResult, secondAddress);
+            if (result.IsFailure)
+                return Result.Fail<Customer>(result.Error);
 
             return Result.Ok(new Customer(name.Value,
                 mainAddress.Value,
                 secondAddress.Value));
+        }
+
+        private static Result Validate<T1, T2>(ValidateableParam<T1> param1, 
+            ValidateableParam<T2> param2, Func<T1, T2, bool> isValidExpression, string errorMessage)
+        {
+            Result param1Result = param1;
+            Result param2Result = param2;
+
+            var combinationResult = Result.Combine(param1Result, param2Result);
+            if (combinationResult.IsFailure)
+                return combinationResult;
+
+            var isValid = isValidExpression(param1.Value, param2.Value);
+
+            return isValid ? Result.Ok() : Result.Fail(errorMessage);
         }
     }
 
@@ -71,13 +114,42 @@ namespace Tests.Solution
             
             var customerResult = Customer.Create(nameResult, mainAddressResult, secondAddressResult);
 
-            var validationResult = Result.Combine(
-                nameResult,
-                mainAddressResult,
-                secondAddressResult,
-                customerResult);
+            nameResult.IsSuccess.Should().BeTrue();
+            mainAddressResult.IsSuccess.Should().BeTrue();
+            secondAddressResult.IsSuccess.Should().BeFalse();
+            customerResult.IsSuccess.Should().BeFalse();
+            customerResult.Error.Should().Be("If name starts with A then street should also start with A., Street Length > 5");
+        }
 
-            validationResult.Error.Should().Be("Street Length > 5, If name starts with A then street should also start with A.");
+        [Test]
+        public void Create_WithInvalidSecondAddress_ShouldReturnAllErrorMessages()
+        {
+            var nameResult = CustomerName.Create("Altmann");
+            var mainAddressResult = Address.Create("Aa", "1");
+            var secondAddressResult = Address.Create("Street2", "2");
+
+            var customerResult = Customer.Create(nameResult, mainAddressResult, secondAddressResult);
+            
+            nameResult.IsSuccess.Should().BeTrue();
+            mainAddressResult.IsSuccess.Should().BeTrue();
+            secondAddressResult.IsSuccess.Should().BeFalse();
+            customerResult.IsSuccess.Should().BeFalse();
+            customerResult.Error.Should().Be("Street Length > 5");
+        }
+
+        [Test]
+        public void Create_WithValidSecondAddressAndValidCombinationOfNameAndMainAddress_ShouldReturnAllErrorMessages()
+        {
+            var nameResult = CustomerName.Create("Altmann");
+            var mainAddressResult = Address.Create("Aaaa", "1");
+            var secondAddressResult = Address.Create("Stree", "2");
+
+            var customerResult = Customer.Create(nameResult, mainAddressResult, secondAddressResult);
+
+            nameResult.IsSuccess.Should().BeTrue();
+            mainAddressResult.IsSuccess.Should().BeTrue();
+            secondAddressResult.IsSuccess.Should().BeTrue();
+            customerResult.IsSuccess.Should().BeTrue();
         }
     }
 }
