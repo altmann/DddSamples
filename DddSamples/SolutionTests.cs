473dee9edc33d9e838ccf331baa24d62d4024a6c
Michael Altmann <mic.altmann@gmail.com>
2019-07-14 19:41:39 +0200

    Change logic and add tests
    Solutions
    - Required error message is supported
    - No Helper method Validate() needed
    - Collect all results from parameters together and return it

--- DddSamples/SolutionTests.cs
+++ DddSamples/SolutionTests.cs
@@ -1,7 +1,6 @@
 using CSharpFunctionalExtensions;
 using FluentAssertions;
 using NUnit.Framework;
-using System;
 
 namespace Tests.Solution
 {
@@ -14,47 +13,41 @@ namespace Tests.Solution
         public string Error => _result.Error;
 
         public bool IsValid => _result.IsSuccess;
-        
+
+        public bool IsValidAndHasValue => IsValid && HasValue;
+
+        public Result Result => _result;
+
+        public bool HasValue => _result.IsSuccess && Value != null;
+
+        public bool HasNoValue => !HasValue;
+
         public ValidateableParam(Result<T> result)
         {
             _result = result;
         }
 
-        public static implicit operator Result<T> (ValidateableParam<T> validateableParam)
+        public ValidateableParam(T value)
         {
-            return validateableParam.IsValid
-                ? Result.Ok(validateableParam.Value)
-                : Result.Fail<T>(validateableParam.Error);
-        }
-
-        public static implicit operator Result(ValidateableParam<T> validateableParam)
-        {
-            return validateableParam.IsValid
-                ? Result.Ok(validateableParam.Value)
-                : Result.Fail<T>(validateableParam.Error);
+            _result = Result.Ok(value);
         }
 
         public static implicit operator ValidateableParam<T>(Result<T> result)
         {
-
-            return result.ToValidateableParam();
-        }
-    }
-
-    public static class Extensions
-    {
-        public static ValidateableParam<T> ToValidateableParam<T>(this Result<T> result)
-        {
             return new ValidateableParam<T>(result);
         }
-    }
 
+        public static implicit operator ValidateableParam<T>(T value)
+        {
+            return new ValidateableParam<T>(value);
+        }
+    }
 
     public class Customer
     {
-        public CustomerName Name { get; private set; }
-        public Address MainAddress { get; private set; }
-        public Address SecondAddress { get; private set; }
+        public CustomerName Name { get; private set; } //Required
+        public Address MainAddress { get; private set; } //Required
+        public Address SecondAddress { get; private set; } //Optional
 
         private Customer(CustomerName name, Address mainAddress, Address secondAddress)
         {
@@ -73,32 +66,29 @@ namespace Tests.Solution
             ValidateableParam<Address> mainAddress,
             ValidateableParam<Address> secondAddress)
         {
-            var nameMainAddressCombinationResult = Validate(name, mainAddress,
-                IfNameStartsWithAThenStreetAlsoShouldStartWithA, 
-                "If name starts with A then street should also start with A.");
-            
-            var result = Result.Combine(nameMainAddressCombinationResult, secondAddress);
+            var nameRequiredResult = Result.Create(name.HasValue, "Customer Name is required");
+            var mainAddressRequiredResult = Result.Create(mainAddress.HasValue, "Main Address is required");
+
+            var nameMainAddressCombinationResult = name.IsValidAndHasValue 
+                && mainAddress.IsValidAndHasValue
+                && !IfNameStartsWithAThenStreetAlsoShouldStartWithA(name.Value, mainAddress.Value)
+                ? Result.Fail("If name starts with A then street should also start with A.")
+                : Result.Ok();
+                
+            var result = Result.Combine(
+                nameRequiredResult,
+                mainAddressRequiredResult,
+                name.Result, 
+                mainAddress.Result, 
+                nameMainAddressCombinationResult, 
+                secondAddress.Result);
+
             if (result.IsFailure)
                 return Result.Fail<Customer>(result.Error);
 
             return Result.Ok(new Customer(name.Value,
                 mainAddress.Value,
                 secondAddress.Value));
-        }
-
-        private static Result Validate<T1, T2>(ValidateableParam<T1> param1, 
-            ValidateableParam<T2> param2, Func<T1, T2, bool> isValidExpression, string errorMessage)
-        {
-            Result param1Result = param1;
-            Result param2Result = param2;
-
-            var combinationResult = Result.Combine(param1Result, param2Result);
-            if (combinationResult.IsFailure)
-                return combinationResult;
-
-            var isValid = isValidExpression(param1.Value, param2.Value);
-
-            return isValid ? Result.Ok() : Result.Fail(errorMessage);
         }
     }
 
@@ -135,6 +125,35 @@ namespace Tests.Solution
             secondAddressResult.IsSuccess.Should().BeFalse();
             customerResult.IsSuccess.Should().BeFalse();
             customerResult.Error.Should().Be("Street Length > 5");
+        }
+
+        [Test]
+        public void Create_WithNullSecondAddress_ShouldReturnAllErrorMessages()
+        {
+            var nameResult = CustomerName.Create("Altmann");
+            var mainAddressResult = Address.Create("Aa", "1");
+            Address secondAddress = null;
+
+            var customerResult = Customer.Create(nameResult, mainAddressResult, secondAddress);
+
+            nameResult.IsSuccess.Should().BeTrue();
+            mainAddressResult.IsSuccess.Should().BeTrue();
+            customerResult.IsSuccess.Should().BeTrue();
+        }
+
+        [Test]
+        public void Create_WithNullMainAddress_ShouldReturnAllErrorMessages()
+        {
+            var nameResult = CustomerName.Create("Altmann");
+            Address mainAddress = null;
+            var secondAddressResult = Address.Create("Stree", "2");
+
+            var customerResult = Customer.Create(nameResult, mainAddress, secondAddressResult);
+
+            nameResult.IsSuccess.Should().BeTrue();
+            secondAddressResult.IsSuccess.Should().BeTrue();
+            customerResult.IsSuccess.Should().BeFalse();
+            customerResult.Error.Should().Be("Main Address is required");
         }
 
         [Test]
